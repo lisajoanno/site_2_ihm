077365eaf88bd73564d9ad9568905b79211e898c
Lisa JOANNO <jl201213@etu.unice.fr>
2015-05-17 22:58:33 +0200

    last commit

--- app/scripts/controllers/services/rolesService.js
+++ app/scripts/controllers/services/rolesService.js
@@ -5,7 +5,11 @@
  */
 angular.module('pooIhmApp').service('Roles',['$http',function Roles($http) {
 
-  // GET ALL
+  /**
+   * GET all roles
+   * @param onSuccess
+   * @param onError
+   */
   this.getAll = function(onSuccess,onError) {
     $http.get('http://poo-ihm-2015-rest.herokuapp.com/api/Roles')
       .success(function(data) {
@@ -17,7 +21,12 @@ angular.module('pooIhmApp').service('Roles',['$http',function Roles($http) {
       });
   };
 
-  // GET d'un id
+  /**
+   * GET d'un role en particulier
+   * @param roleId
+   * @param onSuccess
+   * @param onError
+   */
   this.get = function(roleId,onSuccess,onError) {
     $http.get('http://poo-ihm-2015-rest.herokuapp.com/api/Roles/' + roleId)
       .success(function(data) {
@@ -29,7 +38,12 @@ angular.module('pooIhmApp').service('Roles',['$http',function Roles($http) {
       });
   };
 
-  // DELETE d'un id
+  /**
+   * DELETE d'un role
+   * @param roleId
+   * @param onSuccess
+   * @param onError
+   */
   this.delete = function(roleId,onSuccess,onError) {
     $http.delete('http://poo-ihm-2015-rest.herokuapp.com/api/Roles/' + roleId)
       .success(function(data) {
@@ -41,7 +55,13 @@ angular.module('pooIhmApp').service('Roles',['$http',function Roles($http) {
       });
   };
 
-  // PUT d'un id
+  /**
+   * PUT modification d'un role
+   * @param roleId
+   * @param updateData
+   * @param onSuccess
+   * @param onError
+   */
   this.put = function(roleId,updateData,onSuccess,onError) {
     $http.put('http://poo-ihm-2015-rest.herokuapp.com/api/Roles/' + roleId,updateData)
       .success(function(data) {
@@ -53,7 +73,12 @@ angular.module('pooIhmApp').service('Roles',['$http',function Roles($http) {
       });
   };
 
-  // POST d'un id
+  /**
+   * POST ajout d'un role
+   * @param addData
+   * @param onSuccess
+   * @param onError
+   */
   this.post = function(addData,onSuccess,onError) {
     $http.post('http://poo-ihm-2015-rest.herokuapp.com/api/Roles/',addData)
       .success(function(data) {
@@ -64,10 +89,4 @@ angular.module('pooIhmApp').service('Roles',['$http',function Roles($http) {
         }
       });
   };
-
-
-
-
-
-
 }]);
